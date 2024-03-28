05f5e927ee552a924e3ee883d093fe20c4552ed0
Shan Wijenayaka <shanwije.dev@gmail.com>
2024-03-28 12:09:20 +0800

    tailwind default navbar added

--- src/routes/Root/index.jsx
+++ src/routes/Root/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import Header from '../../components/Header';
 const Root = () => {
   return (
     <>
+        <Header/>
       <div id="detail">
         <Outlet />
       </div>
