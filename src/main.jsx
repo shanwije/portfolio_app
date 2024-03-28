fff2944af4fff2f834a5aec8b750bd684d57661f
Shan Wijenayaka <shanwije.dev@gmail.com>
2024-03-28 10:09:34 +0800

    basic router added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home"
+import NotFound404 from "./pages/NotFound404";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "*",
+    element: <NotFound404 />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/NotFound404/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound404 = () => {
+  return (
+    <div className='place-items-center, text-center'>
+      <h1>Oops 404!</h1>
+    </div>
+  );
+}
+
+export default NotFound404;
