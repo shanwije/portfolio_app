e5c8fc77ae36407dcdfa7bb60475d3de6151d679
Shan Wijenayaka <shanwije.dev@gmail.com>
2024-03-28 18:32:45 +0800

    set default route to home

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "/home",
+        index: true,
         element: <Home />,
       },
     ],
